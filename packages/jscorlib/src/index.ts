01aab6febedec59ad891ef931d18120d3db1db97
forest93 <forest93@163.com>
2024-06-15 17:32:29 +0800

    Update exports

--- packages/jscorlib/src/index.ts
+++ packages/jscorlib/src/index.ts
@@ -1,1 +1,7 @@
-export { };
+export * as Arrays from "./arrays";
+export * as Errors from "./errors";
+export * as Events from "./events";
+export * as Promises from "./promises";
+export * as Strings from "./strings";
+export * as Synchronization from "./synchronization";
+export * as Types from "./types";
