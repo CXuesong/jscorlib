36455f744aa8ec91bd586c9073785bdda0ee063b
forest93 <forest93@163.com>
2024-06-20 01:01:14 +0800

    +asSafeInteger

---
+++ packages/jscorlib/src/numbers/asSafeInteger.ts
@@ -0,0 +1,19 @@
+import { ArgumentRangeError } from "../errors";
+import { SafeInteger } from "./typing";
+
+/**
+ * Truncates the given number into its integer part,
+ * and checks whether the value is within the safe integer range
+ * 
+ * @returns the truncated number, guaranteed to be within the range of
+ * [{@link !Number.MIN_SAFE_INTEGER}, {@link !Number.MAX_SAFE_INTEGER}]
+ * @throws ArgumentRangeError specified `value` cannot be represented by a safe integer.
+ * @see {@link !Number.isSafeInteger}
+ */
+export function asSafeInteger(value: SafeInteger): SafeInteger {
+  const result = Math.trunc(value);
+  if (!Number.isSafeInteger(result)) {
+    throw ArgumentRangeError.create(0, "value", `Value ${value} cannot be represented as an integer without precesion loss.`);
+  }
+  return result;
+}
---
+++ packages/jscorlib/src/numbers/typing.ts
@@ -0,0 +1,5 @@
+/**
+ * A nominal type alias of `number`, which is used to indicate the typed parameter will be
+ * treated as an integer that is within the range of {@link !Number.isSafeInteger | safe integer}.
+ */
+export type SafeInteger = number;
