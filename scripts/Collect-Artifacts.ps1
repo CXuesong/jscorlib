c81f17ed8155ff4a1424b90e2a8cbb3bba2af706
forest93 <forest93@163.com>
2024-07-15 00:40:57 +0800

    Collect artifacts by packages.

--- scripts/Collect-Artifacts.ps1
+++ scripts/Collect-Artifacts.ps1
@@ -13,4 +13,34 @@ Import-Module $PSScriptRoot/Utilities.psm1
 $ArtifactPath = (New-Item $ArtifactPath -ItemType Directory -Force).FullName
 
 Write-Host "ArtifactPath=$ArtifactPath"
-Copy-Item ./packages $ArtifactPath/
+
+Write-Host
+$WorkspacePackagesPath = (New-Item $ArtifactPath/workspace-packages -ItemType Directory -Force).FullName
+Write-Host "WorkspacePackagesPath=$ArtifactPath"
+
+$Packages = yarn workspaces list --json | ConvertFrom-Json
+checkLastExitCode
+
+foreach ($Package in $Packages) {
+    Write-Host "$($Package.name) [$($Package.location)]"
+    $SrcPath = (Resolve-Path $Package.location).Path
+    Write-Host "  $SrcPath"
+
+    $DestRelPath = $Package.location -replace "^packages/", ""
+    $DestPath = (New-Item $WorkspacePackagesPath/$DestRelPath -ItemType Directory -Force).FullName
+    Write-Host "  -> $DestPath"
+
+    function Copy-PackageItem([string]$RelPath, [switch]$Recurse) {
+        if (Test-Path $SrcPath/$RelPath) {
+            Write-Host "  $RelPath"
+            Copy-Item -Recurse:$Recurse $SrcPath/$RelPath $DestPath/
+        }
+    }
+
+    Copy-PackageItem src -Recurse
+    Copy-PackageItem lib -Recurse
+    Copy-PackageItem package.json
+    Copy-PackageItem *.md
+
+    Write-Host
+}
