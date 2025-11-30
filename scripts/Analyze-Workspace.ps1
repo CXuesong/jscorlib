885377c40e7dea9766a0c29d7580f9b7e2370a10
forest93 <forest93@163.com>
2025-11-30 23:59:29 +0800

    tinylibs/tinybench#83 was resolved.

--- scripts/Analyze-Workspace.ps1
+++ scripts/Analyze-Workspace.ps1
@@ -9,6 +9,5 @@ yarn test:coverage:prod
 checkLastExitCode
 
 Write-Host "Run benchmarks"
-Write-Host "Disabled for now; probably due to https://github.com/tinylibs/tinybench/issues/83"
-# yarn bench:prod
-# checkLastExitCode
+yarn bench:prod
+checkLastExitCode
