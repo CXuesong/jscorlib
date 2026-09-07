1ead62f2cc0a767cdcc2dbfd5488aa4f5de716ad
forest93 <forest93@163.com>
2026-09-07 23:27:41 +0800

    Upgrade deps.

--- packages/@jscorlib-repo/gh-pages-hosting/vite.config.ts
+++ packages/@jscorlib-repo/gh-pages-hosting/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import { checker } from "vite-plugin-checker";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tsconfigPaths({ loose: true }),
     checker({
       typescript: {
         tsconfigPath: "./src/tsconfig.json",
@@ -15,6 +13,9 @@ export default defineConfig({
     svelte(),
   ],
   base: "./",
+  resolve: {
+    tsconfigPaths: true,
+  },
   build: {
     outDir: "./dist",
     emptyOutDir: true,
